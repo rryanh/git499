19c00a4af7f04214c5ee1f1cabbe82a47e28c04e
rryanh <ryanhuga@gmail.com>
2020-09-15 09:59:42 -0700

    started assignment

---
+++ src/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args){
+       featureA();
+    }
+    //first feature of program
+    public static void featureA(){
+        System.out.println("Feature A");
+    }
+}
