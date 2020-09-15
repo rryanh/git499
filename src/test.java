1ee1a07aa56a205a9dba0c6a41cad5310f022fd2
rryanh <ryanhuga@gmail.com>
2020-09-15 10:08:59 -0700

    intial commit

--- src/test.java
+++ src/test.java
@@ -2,7 +2,8 @@ public class test {
     public static void main(String[] args){
        featureA();
     }
-    //first feature of program
+    //Base feature of program
+    //feature B & C still needed
     public static void featureA(){
         System.out.println("Feature A");
     }
