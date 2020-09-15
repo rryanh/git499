2ed8846a6e39a8c080810e48836f698bcab50b79
rryanh <ryanhuga@gmail.com>
2020-09-15 10:11:43 -0700

    Implemented Feature B

--- src/test.java
+++ src/test.java
@@ -1,10 +1,17 @@
+import java.awt.*;
+
 public class test {
     public static void main(String[] args){
        featureA();
+       featureB();
     }
     //Base feature of program
-    //feature B & C still needed
+    //feature C still needed
     public static void featureA(){
         System.out.println("Feature A");
+    }
+    //feature B
+    public static void featureB(){
+        System.out.println("Feature B");
     }
 }
